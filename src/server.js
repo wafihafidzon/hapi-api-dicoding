dfcc6fab72ffeb0148fb58995ed5e0b1cd080005
wafihafidzon <wafipiq02@gmail.com>
2023-06-09 07:39:46 +0700

    edit port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
 const init = async () => {
   const server = Hapi.server({
-    port: 5001,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
